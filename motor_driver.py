33397c60a1a7a328cab274ee60675f9c5a75651b
aaron-esc-13 <156145840+aaron-esc-13@users.noreply.github.com>
2024-02-05 14:18:51 -0800

    working motor driver

--- motor_driver.py
+++ motor_driver.py
@@ -1,33 +1,33 @@
 
 import micropython
 import utime
-
 class MotorDriver:
     """! 
     This class implements a motor driver for an ME405 kit. 
     """
 
-    def __init__ (self, en_pin, in1pin, in2pin, timer):
+    def __init__ (self):
         """! 
         Creates a motor driver by initializing GPIO
         pins and turning off the motor for safety. 
         @param en_pin (There will be several parameters)
         """
-        print ("Creating a motor driver")
+        pin_ENA = pyb.Pin(pyb.Pin.cpu.A10, pyb.Pin.OUT_PP)
         
-        pin_ENA = pyb.Pin(pyb.Pin.board.PINA10, pyb.Pin.OUT_PP)
-        
-        pin1A = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
-        tim3 = pyb.Timer(3, freq=500)
+        pin1A = pyb.Pin.cpu.B4
+        tim3 = pyb.Timer(3, freq=20000)
         ch1 = tim3.channel(1, pyb.Timer.PWM, pin=pin1A)
         
-        pin2A = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
-        tim3 = pyb.Timer(3, freq=500)
+        pin2A = pyb.Pin.cpu.B5
+        tim3 = pyb.Timer(3, freq=20000)
         ch2 = tim3.channel(2, pyb.Timer.PWM, pin=pin2A)
         
-        return ch1, ch2
+        self.ch1 = ch1
+        self.ch2 = ch2
+        
+        print ("Creating a motor driver")
 
-    def set_duty_cycle (self, level):
+    def set_duty_cycle (self,level1,level2):
         """!
         This method sets the duty cycle to be sent
         to the motor to the given level. Positive values
@@ -36,12 +36,9 @@ class MotorDriver:
         @param level A signed integer holding the duty
                cycle of the voltage sent to the motor 
         """
-        print (f"Setting duty cycle to {level}")
-
-        ch.pulse_width_percent(x)
+        print (f"Setting duty cycle to {level1}")
+        self.ch1.pulse_width_percent(level1)
+        self.ch2.pulse_width_percent(level2)
         
-    ch1, ch2 = __init__(self, pin_ENA, pin1A, pin2A, timer)
-
-    while True:
-        set_duty_cycle(self, 50)
-    
+motor = MotorDriver()
+motor.set_duty_cycle(100,0)
